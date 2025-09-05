8272c5d5786cde38fafee87a1b759180bdbd209c
hsyoon_kccistc <dbsghtmd99@gmail.com>
2025-09-05 16:23:38 +0900

    Update tb

--- 250829_counter_fnd_review/sim/tb_count_10000.v
+++ 250829_counter_fnd_review/sim/tb_count_10000.v
@@ -2,23 +2,29 @@
 module tb_count_10000();
     reg clk, rst;
     reg mode;
-    reg enable, clear;
+    reg clear;
     wire [$clog2(10000)-1:0] count;
 
     counter_10000 uut (
         .clk (clk),
         .rst (rst),
-        .mode (mode), // 0: down, 1: up
+        .i_tick (1'b1),
+        .mode (mode), // 0: up, 1: down
+        .clear(clear),
         .count_reg (count)
     );
 
     always #5 clk = ~clk;
 
     initial begin
-        #0 clk=0; rst=1; mode=1; enable=1; clear=0;
-        #10 rst=0;
+        #0; clk = 0; rst = 1; mode = 0; clear = 0;
+        #10; rst = 0;
+        #1000; mode = 1;
+        #1500; clear = 1;
+        #10; clear = 0;
+        #100; mode = 0;
 
-        #100000
+        #10000;
     $finish;
     end
 endmodule
